13fd8e5edad0348114385c5ac007a111499e1980
DarkHost <sultanisk@mail.ru>
2024-08-04 20:19:36 +0500

    Initial Commit

---
+++ ForeCast/ForeCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForeCastApp.swift
+//  ForeCast
+//
+//  Created by Sultan on 04.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct ForeCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
